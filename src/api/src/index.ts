7b2f9dccfff77e0fe63b9e873bc58949e5069ec8
Ravi Narsini <ravikumar.narsini@audreeinfotech.com>
2024-11-27 16:41:40 +0530

    port changed to 80

--- src/api/src/index.ts
+++ src/api/src/index.ts
@@ -3,7 +3,7 @@ import { logger } from "./config/observability";
 
 const main = async () => {
     const app = await createApp();
-    const port = 3100;
+    const port = 80;
 
     app.listen(port, () => {
         logger.info(`Started listening on port ${port}`);
